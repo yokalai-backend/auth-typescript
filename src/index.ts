eac9d0328610e9c26dc9000480008832b5f961e9
Yoka Lai <yokalai910@gmail.com>
2026-03-13 21:20:06 +0700

    feat: server setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello this is branch");
+//Use express for making the server.
+import { config } from "dotenv";
+config();
+
+import express from "express";
+import cors from "cors";
+
+//Use server middleware by combine express and cors for server.
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+//Run the server
+const PORT = process.env.SERVER_PORT;
+app.listen(PORT, () => {
+  console.log("Server is running at port ", PORT);
+});
