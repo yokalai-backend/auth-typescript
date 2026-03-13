ef52232519ddb3937af33eb6c3285439fbd61ba9
Yoka Lai <yokalai910@gmail.com>
2026-03-13 23:25:57 +0700

    feat: add some moddified for reg

--- src/controllers/auth/register.ts
+++ src/controllers/auth/register.ts
@@ -1,7 +1,14 @@
 //Register async function
 import type { ControllersParameters } from "../../types/req.res.next";
+import registerService from "../../services/auth/register";
 
 export default async function registerAuth({
   req,
   res,
-}: ControllersParameters) {}
+}: ControllersParameters) {
+  const result = await registerService({ ...req.body });
+
+  return res
+    .status(201)
+    .json({ success: true, message: "Account created", data: result });
+}
